295cf02252b2077492267d14c61fbf8f8c2e16dd
Tim Gremalm <tim@gremalm.se>
2015-01-18 13:53:27 +0100

    Added functions to add description to georg

--- cronjob/cronjob.py
+++ cronjob/cronjob.py
@@ -2,12 +2,21 @@
 import json
 import urllib2
 georgUrl = "http://localhost:3000/collectionapi/events"
+georgEvents = []
 
 def main():
 	print("main")
-	georgPut()
+	georgGetAll()
+	#georgPut()
+	georgCheckAndPut("Beskrivningyo", 57, 12)
 
-def georgPut():
+def georgCheckAndPut(description, lat, lon):
+	for value in georgEvents:
+		if description == value["description"]:
+			return
+	georgPut(description, lat, lon)
+
+def georgPut(description, lat, lon):
 	#[{
 	#	"_id":"LEshFkwmcyLy86qS2",
 	#	"description":"Beskrivning",
@@ -16,9 +25,9 @@ def georgPut():
 	#	"visible":false
 	#}]
 	data = {
-		"description": "Beskrivning1",
-		"lat": 57,
-		"lon": 12,
+		"description": description,
+		"lat": lat,
+		"lon": lon,
 		"visible": True
 	}
 
@@ -27,7 +36,15 @@ def georgPut():
 
 	response = urllib2.urlopen(req, json.dumps(data))
 
-	print response
+def georgGetAll():
+	global georgEvents
+
+	req = urllib2.Request(georgUrl)
+	response = urllib2.urlopen(req)
+
+	georgEvents = json.load(response)
+
+	#print georgEvents
 
 if __name__ == '__main__':
 	#Call Main-function
