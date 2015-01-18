9f33e462207d035897608a60ca014287abde2829
Tim Gremalm <tim@gremalm.se>
2015-01-18 05:51:37 +0100

    Started on the json-post part

--- cronjob/cronjob.py
+++ cronjob/cronjob.py
@@ -1,2 +1,36 @@
 #!/usr/bin/env python
+import json
+import urllib2
+georgUrl = "http://localhost:3000/collectionapi/events"
+
+def main():
+	print("main")
+	georgPut()
+
+def georgPut():
+	#[{
+	#	"_id":"LEshFkwmcyLy86qS2",
+	#	"description":"Beskrivning",
+	#	"lat":57,
+	#	"lon":12,
+	#	"visible":false
+	#}]
+	data = {
+		"description": "Beskrivning1",
+		"lat": 57,
+		"lon": 12,
+		"visible": True
+	}
+
+	req = urllib2.Request(georgUrl)
+	req.add_header('Content-Type', 'application/json')
+
+	response = urllib2.urlopen(req, json.dumps(data))
+
+	print response
+
+if __name__ == '__main__':
+	#Call Main-function
+	main()
+
 
